71d7e95624494fab64b047b74582a7012a85b33c
vijay18399 <vijayreddy18399@gmail.com>
2020-07-27 11:34:30 +0530

    added

--- bee_socket/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtSecret: 'some secret ',
-    db: 'mongodb://vijay18399:dilse18399@ds351428.mlab.com:51428/users'
-};
--- tutorial/models/hallticket.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var HallticketSchema = new Schema({
-  roll_number: String,
-  subjects: Object,
-  name : String
-});
-module.exports = mongoose.model('Hallticket', HallticketSchema);
